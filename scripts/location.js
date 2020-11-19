87b4da415ef639d189ad3845cf3dc4a6be126b02
Jordan Carter <jordan.carter.0023@gmail.com>
2020-11-19 13:42:46 -0600

    fixed location url button

--- scripts/location.js
+++ scripts/location.js
@@ -4,7 +4,7 @@ export const Location = (location) => {
         <section class="location card">
             <div><img  class="location__image image--card" src="${location.image}" /></div>
             <div class="location__name">${location.name}</div>
-            <div class="location__url">${location.url}</div>
+            <div class="location__url"> <a href=${location.url}target="_blank" rel="noopener noreferrer"> LEARN MORE HERE</a></div>
             <div class="location__description">${location.description}</div>
         </section>
     `
