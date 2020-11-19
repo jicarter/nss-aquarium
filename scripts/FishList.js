35e969a2a2d0d3ed17c83ff24f8e5a3f0e9baed0
Jordan Carter <jordan.carter.0023@gmail.com>
2020-11-19 12:56:10 -0600

    added filter functions

--- scripts/FishList.js
+++ scripts/FishList.js
@@ -1,13 +1,34 @@
-import { useFish } from './fishDataProvider.js'
+import { mostHolyFish } from './fishDataProvider.js'
+import { nonHolyFish } from './fishDataProvider.js'
+import { soldierFish } from './fishDataProvider.js'
 import { Fish } from './fish.js'
+
 
 export const FishList = () => {
 
     // Get a reference to the `<article class="content">` element
     const contentElement = document.querySelector(".fishList")
-    const allTheFish = useFish()
+    const holyFish = mostHolyFish()
+    const soldiers = soldierFish()
+    const losers = nonHolyFish()
 
-    for (const fishObject of allTheFish) {
+    for (const fishObject of holyFish) {
+    
+        const fishHTML = Fish(fishObject)
+    
+    // Add to the existing HTML in the content element
+    contentElement.innerHTML += fishHTML
+        
+    }  
+    for (const fishObject of soldiers) {
+    
+        const fishHTML = Fish(fishObject)
+    
+    // Add to the existing HTML in the content element
+    contentElement.innerHTML += fishHTML
+        
+    }  
+    for (const fishObject of losers) {
     
         const fishHTML = Fish(fishObject)
     
